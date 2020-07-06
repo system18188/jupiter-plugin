21c769efb03fbee740bfade90b7a8c2e24eb8e39
netbo <system18188@gmail.com>
2020-07-06 09:56:10 +0800

    update

--- store/dbr/update.go
+++ store/dbr/update.go
@@ -3,7 +3,11 @@ package dbr
 import (
 	"context"
 	"database/sql"
+	"github.com/system18188/jupiter-plugin/pkg/check"
+	"reflect"
 	"strconv"
+	"strings"
+	"time"
 )
 
 // UpdateStmt builds `UPDATE ...`.
@@ -171,6 +175,57 @@ func (b *UpdateStmt) Set(column string, value interface{}) *UpdateStmt {
 func (b *UpdateStmt) SetMap(m map[string]interface{}) *UpdateStmt {
 	for col, val := range m {
 		b.Set(col, val)
+	}
+	return b
+}
+
+// 扫描struct按column绑定值 (传入值, 表列名) tag 绑定db名
+func (b *UpdateStmt) Scan(formStruct interface{}, columns ...string) *UpdateStmt {
+	tyof := reflect.TypeOf(formStruct)
+	vaof := reflect.ValueOf(formStruct)
+	if tyof.Kind() == reflect.Ptr {
+		tyof = tyof.Elem()
+		vaof = vaof.Elem()
+	}
+	if tyof.Kind() != reflect.Struct {
+		return b
+	}
+	columnlen := len(columns)
+	for i := 0; i < tyof.NumField(); i++ {
+		typeField := tyof.Field(i)  // struct取行类型
+		valueField := vaof.Field(i) // struct取行值
+		// 取列名
+		columnName := typeField.Tag.Get("db")
+		formName := typeField.Tag.Get("form")
+		columnNameList := strings.Split(columnName, ",")
+		formNameList := strings.Split(formName, ",")
+		columnName = columnNameList[0]
+		formName = formNameList[0]
+		// 列名为空转到下一行
+		if columnName == "" || formName == "" || columnName == "-" || formName == "-" {
+			continue
+		}
+		// 检查类名是否存在
+		if columnlen > 0 && !check.IsSliceContainsString(formName, columns...) {
+			continue
+		}
+		switch typeField.Type.Kind() {
+		case reflect.String:
+			b.Set(columnName, valueField.String())
+		case reflect.Int, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int8:
+			b.Set(columnName, valueField.Int())
+		case reflect.Bool:
+			b.Set(columnName, valueField.Bool())
+		case reflect.Float32, reflect.Float64:
+			b.Set(columnName, valueField.Float())
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			b.Set(columnName, valueField.Uint())
+		default:
+			if now, isTime := valueField.Interface().(time.Time); isTime {
+				b.Set(columnName, now)
+				continue
+			}
+		}
 	}
 	return b
 }
